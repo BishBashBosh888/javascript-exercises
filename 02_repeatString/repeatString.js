2219e7f26d60e87a9973def15d44e4c1fab6cded
fsut0005 <fsut0005@student.monash.edu>
2024-12-19 20:13:36 +1100

    exercise 1 and 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    if(number < 0){
+        return 'ERROR';
+    }else{
+        i = 0
+        temp = "";
+        while(i < number){
+            temp = temp + word;
+            i++;
+        }
+        return temp;
+    }
 };
 
 // Do not edit below this line

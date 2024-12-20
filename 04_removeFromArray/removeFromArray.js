4ae28f2e95abc8fe1bf940bfc8c5a5fef3fd613b
fsut0005 <fsut0005@student.monash.edu>
2024-12-20 16:02:35 +1100

    exercise 4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    temp = [];
+    arr.forEach(num => {
+        if(!nums.includes(num)){
+            temp.push(num);
+        }
+    });
+    return temp
 };
 
 // Do not edit below this line

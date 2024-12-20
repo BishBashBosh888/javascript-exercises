9c68dab50c951c80df22723e343d717732f49761
fsut0005 <fsut0005@student.monash.edu>
2024-12-20 21:30:30 +1100

    exercise 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    if(num1 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
+        return 'ERROR';
+    }
 
+    if(num2 < num1){
+        let temp;
+        temp = num2;
+        num2 = num1;
+        num1 = temp;      
+    }
+    sum = 0;
+
+    for(let i = num1; i <= num2; i++){
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
